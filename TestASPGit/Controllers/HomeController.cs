e6fde5f282255f7a872c267841933ceedca299e4
kamel-rush <kamel@grandcircus.co>
2017-10-30 11:47:25 -0400

    changed the About action

--- TestASPGit/Controllers/HomeController.cs
+++ TestASPGit/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TestASPGit.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Welcome!";
 
             return View();
         }
